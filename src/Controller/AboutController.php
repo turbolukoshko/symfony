6c4b7a01462e3f2b0f091e90a3563c110715b3ee
Vladislav <turbo.lukoshko@gmail.com>
2018-05-09 16:45:30 +0300

    upd

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -16,4 +16,44 @@ class AboutController extends Controller
             'title_about' => 'Pollena Minima | About',
         ]);
     }
+
+    /**
+     * @Route("/news1", name="news1")
+     */
+    public function news1()
+    {
+        return $this->render('about/news1.html.twig', [
+            'title_news' => 'About | News',
+        ]);
+    }
+
+    /**
+     * @Route("/news2", name="news2")
+     */
+    public function news2()
+    {
+        return $this->render('about/news2.html.twig', [
+            'title_news' => 'About | News',
+        ]);
+    }
+
+    /**
+     * @Route("/news3", name="news3")
+     */
+    public function news3()
+    {
+        return $this->render('about/news3.html.twig', [
+            'title_news' => 'About | News',
+        ]);
+    }
+
+    /**
+     * @Route("/news4", name="news4")
+     */
+    public function news4()
+    {
+        return $this->render('about/news4.html.twig', [
+            'title_news' => 'About | News',
+        ]);
+    }
 }
--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -16,4 +16,47 @@ class BlogController extends Controller
             'title_blog' => 'Pollena Minima | Blog',
         ]);
     }
+
+    /**
+     * @Route("/article1", name="article1")
+     */
+    public function article1()
+    {
+        return $this->render('blog/article1.html.twig', [
+            'title_article' => 'Blog | Article',
+        ]);
+    }
+
+    /**
+     * @Route("/article2", name="article2")
+     */
+    public function article2()
+    {
+        return $this->render('blog/article2.html.twig', [
+            'title_article' => 'Blog | Article',
+        ]);
+    }
+
+    /**
+     * @Route("/article3", name="article3")
+     */
+    public function article3()
+    {
+        return $this->render('blog/article3.html.twig', [
+            'title_article' => 'Blog | Article',
+        ]);
+    }
+
+    /**
+     * @Route("/article4", name="article4")
+     */
+    public function article4()
+    {
+        return $this->render('blog/article4.html.twig', [
+            'title_article' => 'Blog | Article',
+        ]);
+    }
+
+
+
 }
